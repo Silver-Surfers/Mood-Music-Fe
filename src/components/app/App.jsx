4e2dbf592d6e4a2ad87a1045d7bbe311b2ef19d4
Brian McPhillips <B.McPhillips89@gmail.com>
2020-11-11 13:16:04 -0800

    Camera working and taking photo

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { WebcamCapture } from '../../../Sandbox/CameraSandbox/CameraSandbox';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <WebcamCapture /> ;
 }
