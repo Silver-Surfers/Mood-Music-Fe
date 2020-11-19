07d5629ece722f503a03863a95b37ae546e36b86
Jerud Moyer <jerudmoyer@gmail.com>
2020-11-19 10:20:16 -0800

    Graph (#7)
    * were geniuses
    
    * graph added some css some bug fixes

---
+++ src/components/WebCam/Graph.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  PieChart, Pie, Sector, Cell,
+} from 'recharts';
+import { useSelector } from 'react-redux';
+import { selectEmotion } from '../../selectors/azureSelectors';
+
+export const Graph = () => {
+  
+  const {
+    anger,
+    contempt,
+    disgust,
+    fear,
+    happiness,
+    neutral,
+    sadness,
+    surprise 
+  } = useSelector(selectEmotion);
+
+  const data = [
+    { name: 'Anger', value: anger * 100 },
+    { name: 'Contempt', value: contempt * 100 },
+    { name: 'Disgust', value: disgust * 100 },
+    { name: 'Fear', value: fear * 100 },
+    { name: 'Happiness', value: happiness * 100 },
+    { name: 'Neutral', value: neutral * 100 },
+    { name: 'Sadness', value: sadness * 100 },
+    { name: 'Surprise', value: surprise * 100 }
+  ];
+
+  const COLORS = ['#FF0000',
+    '#990099',
+    '#660099',
+    '#FFCC00',
+    '#66CC00',
+    '#e6e6da',
+    '#3399FF',
+    '#ECEC46'];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx, cy, midAngle, innerRadius, outerRadius, percent, index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  
+    return (
+      <text x={x} y={y}
+        fill="black"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+
+
+  
+
+    
+  return (
+    <PieChart width={400} height={250}>
+      <Pie
+        data={data}
+        cx={200}
+        cy={120}
+        labelLine={false}
+        label={renderCustomizedLabel}
+        outerRadius={100}
+        fill="#8884d8"
+        dataKey="value"
+      >
+        {
+          data.map((
+            entry,
+            index) => <Cell
+            key={`cell-${index}`}
+            fill={COLORS[index % COLORS.length]} 
+          />)
+        }
+      </Pie>
+    </PieChart>
+  );
+};
