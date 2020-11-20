913cbb252b920913177ca1a704addb9332dd86a0
Jerud Moyer <jerudmoyer@gmail.com>
2020-11-19 17:18:31 -0800

    About page (#11)
    * about us page added - styling needed
    
    * apostrophes corrected/added
    
    * minor adjustments
    
    * all data placed - styling needed
    
    Co-authored-by: Annalise Murphy <nali247@gmail.com>
    Co-authored-by: Brian McPhillips <B.McPhillips89@gmail.com>

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,82 +1,31 @@
-/* eslint-disable max-len */
+import { names } from '../Data/data';
 import React from 'react';
-import AnnaliseMurphy from '../Images/AnnaliseMurphy.jpeg';
-import BrianMcPhillips from '../Images/BrianMcPhillips.jpg';
-import JerudMoyer from '../Images/JerudMoyer.jpg';
-import NateKruck from '../Images/NateKruck.png';
-import github from '../Images/github.png';
-import linkedIn from '../Images/linkedIn.jpg';
+import github from '../Data/Images/github.png';
+import linkedIn from '../Data/Images/linkedIn.jpg';
 
 export const AboutPage = () => {
+  const nameStuff = names.map(name => (
+    <li key={name.name}>
+      <img src={name.image} alt={name.name} />
+      <h2>{name.name}</h2>
+      <p>{name.description}</p>
+      <div className="links">
+        <a href={name.github}>
+          <img src={github} alt={`${name.name} github`}></img>
+        </a>
+        <a href={name.linkedin}>
+          <img src={linkedIn} alt={`${name.name} linkedIn`}></img>
+        </a>
+      </div>
+    </li>
+  ));
 
   return (
     <>
-      <h2>About Us</h2>
-      <div className="main-container">
-        <div className="jerud-container">
-          <img src={JerudMoyer} alt="Jerud" />
-          <h3>Jerud Moyer</h3>
-          <p>
-            Jerud is a full-stack software developer and dedicated musician. When he&#39;s feeling happy, sad or otherwise you can probably find him listening to Black Sabbath.
-          </p>
-          <div className="links">
-            <a href="https://github.com/Jerud-Moyer">
-              <img src={github}alt="Jerud-Github"></img>
-            </a>
-            <a href="https://www.linkedin.com/in/jerud-moyer/">
-              <img src={linkedIn} alt="Jerud-LinkedIn"></img>
-            </a>
-          </div>
-        </div>
-
-        <div className="brian-container">
-          <img src={BrianMcPhillips} alt="Brian" />
-          <h3>Brian McPhillips</h3>
-          <p>
-          Brian McPhillips Full Stack JavaScript developer I am always listening to different types of electronic music and making full stack apps.
-          </p>
-          <div className="links">
-            <a href="https://github.com/BrianMcPhillips">
-              <img src={github}alt="Brian-Github"></img>
-            </a>
-            <a href="https://www.linkedin.com/in/brianmcphillips">
-              <img src={linkedIn} alt="Brian-LinkedIn"></img>
-            </a>
-          </div>
-        </div>
-
-        <div className="nate-container">
-          <img src={NateKruck} alt="Nate" />
-          <h3>Nate Kruck</h3>
-          <p>
-            Full Stack Software Engineer. I like upbeat music no matter what mood I&#39;m in! Hit me with some EDM and you&#39;ll catch me going wild!
-          </p>
-          <div className="links">
-            <a href="https://github.com/Nate-Kruck">
-              <img src={github}alt="Nate-Github"></img>
-            </a>
-            <a href="https://www.linkedin.com/in/natekruck/">
-              <img src={linkedIn} alt="Nate-LinkedIn"></img>
-            </a>
-          </div>
-        </div>
-
-        <div className="annalise-container">
-          <img src={AnnaliseMurphy} alt="Annalise" />
-          <h3>Annalise Murphy</h3>
-          <p>
-            Annalise is a software engineer, playlist curator and an avid music lover! When she&#39;s not jamming out Black Sabbeth, she&#39;s busy building Full Stack apps.
-          </p>
-          <div className="links">
-            <a href="https://github.com/Annalise-M">
-              <img src={github}alt="Annalise-Github"></img>
-            </a>
-            <a href="https://www.linkedin.com/in/annalise-murphy/">
-              <img src={linkedIn} alt="Annalise-LinkedIn"></img>
-            </a>
-          </div>
-        </div>
-      </div>
+      <h1>About Us</h1>
+      <ul className="list">
+        {nameStuff}
+      </ul>
     </>
   );
 };
