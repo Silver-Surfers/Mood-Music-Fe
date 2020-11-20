83f2d9f261e3cb12e95eb7a3aa62db68d8df6430
Jerud-Moyer <jerudmoyer@gmail.com>
2020-11-19 20:33:04 -0800

    fixin

--- src/components/AuthPage.jsx
+++ src/components/AuthPage.jsx
@@ -1,26 +1,12 @@
 /* eslint-disable max-len */
 import React from 'react';
 import styles from './AuthPage.css';
-import spotify from  '../images/spotify.png';
-// import PropTypes from 'prop-types';
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { setToken } from '../actions/spotifyActions';
-// import { setLoading } from '../actions/azureActions';
+import spotify from  '../Data/Images/spotify.png';
+
 
 export const AuthPage = () => {
-  // console.log(location);
-  // const { search } = location;
-  // const token = search.slice(6, search.length);
-  
-  // const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   dispatch(setToken(token));
-  //   dispatch(setLoading(false));
-  // }, []);
   
-
   return (
     <>
       <div className={styles.flex}>
@@ -40,6 +26,3 @@ export const AuthPage = () => {
     </>
   );
 };
-// AuthPage.propTypes = {
-//   location: PropTypes.string.isRequired
-// }; 
