57947c74f2b665cb0fcbc2fc82ff400fd6f7aeb1
Jerud-Moyer <jerudmoyer@gmail.com>
2020-11-19 17:22:26 -0800

    ok

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -6,20 +6,21 @@ import JerudMoyer from '../Images/JerudMoyer.jpg';
 import NateKruck from '../Images/NateKruck.png';
 import github from '../Images/github.png';
 import linkedIn from '../Images/linkedIn.jpg';
+import styles from './AboutPage.css';
 
 export const AboutPage = () => {
 
   return (
     <>
-      <h2>About Us</h2>
-      <div className="main-container">
-        <div className="jerud-container">
-          <img src={JerudMoyer} alt="Jerud" />
+      
+      <div className={styles.mainContainer}>
+        <div className={styles.jerudContainer}>
+          <img className={styles.devImg} src={JerudMoyer} alt="Jerud" />
           <h3>Jerud Moyer</h3>
           <p>
             Jerud is a full-stack software developer and dedicated musician. When he&#39;s feeling happy, sad or otherwise you can probably find him listening to Black Sabbath.
           </p>
-          <div className="links">
+          <div className={styles.link}>
             <a href="https://github.com/Jerud-Moyer">
               <img src={github}alt="Jerud-Github"></img>
             </a>
@@ -29,13 +30,13 @@ export const AboutPage = () => {
           </div>
         </div>
 
-        <div className="brian-container">
-          <img src={BrianMcPhillips} alt="Brian" />
+        <div className={styles.brianContainer}>
+          <img className={styles.devImg} src={BrianMcPhillips} alt="Brian" />
           <h3>Brian McPhillips</h3>
           <p>
           Brian McPhillips Full Stack JavaScript developer I am always listening to different types of electronic music and making full stack apps.
           </p>
-          <div className="links">
+          <div className={styles.link}>
             <a href="https://github.com/BrianMcPhillips">
               <img src={github}alt="Brian-Github"></img>
             </a>
@@ -45,13 +46,13 @@ export const AboutPage = () => {
           </div>
         </div>
 
-        <div className="nate-container">
-          <img src={NateKruck} alt="Nate" />
+        <div className={styles.natContainer}>
+          <img className={styles.devImg} src={NateKruck} alt="Nate" />
           <h3>Nate Kruck</h3>
           <p>
             Full Stack Software Engineer. I like upbeat music no matter what mood I&#39;m in! Hit me with some EDM and you&#39;ll catch me going wild!
           </p>
-          <div className="links">
+          <div className={styles.link}>
             <a href="https://github.com/Nate-Kruck">
               <img src={github}alt="Nate-Github"></img>
             </a>
@@ -61,13 +62,13 @@ export const AboutPage = () => {
           </div>
         </div>
 
-        <div className="annalise-container">
-          <img src={AnnaliseMurphy} alt="Annalise" />
+        <div className={styles.annaliseContainer}>
+          <img className={styles.devImg} src={AnnaliseMurphy} alt="Annalise" />
           <h3>Annalise Murphy</h3>
           <p>
             Annalise is a software engineer, playlist curator and an avid music lover! When she&#39;s not jamming out Black Sabbeth, she&#39;s busy building Full Stack apps.
           </p>
-          <div className="links">
+          <div className={styles.link}>
             <a href="https://github.com/Annalise-M">
               <img src={github}alt="Annalise-Github"></img>
             </a>
